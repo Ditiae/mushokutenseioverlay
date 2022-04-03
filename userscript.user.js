6e65833915def73ca891b9e7278e8b7a7b4c7abc
Wealth <30428646+Ditiae@users.noreply.github.com>
2022-04-03 05:58:20 -0500

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         for r/place2
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the canvas!
 // @author       KTibow
 // @match        https://hot-potato.reddit.com/*
@@ -12,7 +12,7 @@
 window.addEventListener("load", () => {
     const container = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-canvas").shadowRoot.querySelector(".container");
     const overlay = document.createElement("img");
-    overlay.src = "https://cdn.discordapp.com/attachments/390312001403879424/960110646610001990/rezero.png";
+    overlay.src = "https://cdn.discordapp.com/attachments/901309558016864298/960131077744234596/rezero.png";
     overlay.style = "position: absolute; left: 1867px; top: 799px; width: 32px; pointer-events: none; image-rendering: pixelated;";
     window.i = 0;
     setInterval(() => {
