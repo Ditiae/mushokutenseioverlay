fc462c5255b4bca79b24b1c71896d1fa5c78b72d
Wealth <30428646+Ditiae@users.noreply.github.com>
2022-04-03 19:52:09 -0500

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         for r/place2
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  try to take over the canvas!
 // @author       KTibow
 // @match        https://hot-potato.reddit.com/*
